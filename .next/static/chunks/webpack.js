11a309f2dfcc8424932ec1604e45cf6f0100f1ff
Son Pham Cao <phamcaoson1999@gmail.com>
2024-04-19 14:11:43 +0700

    integrate Payload headless cms

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4233b262bf34838"; }
+/******/ 		__webpack_require__.h = function() { return "a3250de198f4f98f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const { withPayload } = require('@payloadcms/next/withPayload')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPayload(withPayload(nextConfig))
